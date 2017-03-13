7fdb5a4b7fd1391eec9b006f87e1a95fc2cce4d0
ducvuong12 <ducvuong17895@gmail.com>
2017-03-13 14:33:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+vuongminhduc
+</head>
+<body>
+
+<form method="post" action="ve.php">
+	<p> chiều rộng:</p>
+	<input type="text" style="width: 30px" name="r" value=""/>
+	<p> chiều cao:</p>
+	<input type="text" style="width: 30px" name="c" value=""/>
+	<br/><br/>
+	<input type="submit" name="ve" value="tinh"/>
+</table>
+</form>
+</body>
+</html>
